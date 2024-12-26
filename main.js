bbb8f12bab5b07c8f90c476290c50a6555f1e34a
Koushav Sarkar <koushav@itobuz.com>
2024-12-26 18:42:06 +0530

    task completed

---
+++ main.js
@@ -0,0 +1,16 @@
+function aqua () {
+  document.body.style.backgroundColor = "aqua";
+}
+
+function green () {
+  document.body.style.backgroundColor = "green";
+}
+
+function bisque () {
+  document.body.style.backgroundColor = "bisque";
+}
+
+function grey () {
+  document.body.style.backgroundColor = "gray";
+}
+
